b1f31a03871d836c68acb675a036bc352a215214
yanfeizuo <18856856693@163.com>
2025-05-07 09:43:13 +0800

    update healthClient

--- src/constants/index.js
+++ src/constants/index.js
@@ -53,7 +53,7 @@ let wmtrAddr = '0x160361ce13ec33C993b5cCA8f62B6864943eb083'.toLowerCase()
 
 // let client = 'https://graph-meter.voltswap.finance/subgraphs/name/meterio/voltswapv2-subgraph'
 let client = 'https://graph.meter.io/subgraphs/name/meterio/voltswapv2-subgraph'
-let healthClient = 'https://graph-meter.voltswap.finance:8031/graphql'
+let healthClient = 'http://graph-meter.voltswap.finance:8030/graphql'
 // let healthClient = 'https://graph.meter.io:8031/graphql'
 let blockClient = 'https://graph-meter.voltswap.finance/subgraphs/name/meter/blocks'
 // let blockClient = 'https://graph.meter.io/subgraphs/name/meter/blocks'
